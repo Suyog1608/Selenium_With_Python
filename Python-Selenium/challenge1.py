170cf1b637758f51a3e43cb9f1889c3fa8e1e52f
Suyog1608 <gadhavesuyog@gmail.com>
2024-03-12 23:31:29 +0530

    updated the challenge1 file

--- Python-Selenium/challenge1.py
+++ Python-Selenium/challenge1.py
@@ -9,19 +9,13 @@ find_id = driver.find_element(by=By.ID, value="td-block-0")
 print("the id element is:")
 print(find_id)
 
-time.sleep(5)
-
 find_class_name = driver.find_element(by=By.CLASS_NAME, value="no-js")
 print("the class element is: ")
 print(find_class_name)
 
-time.sleep(5)
-
 find_head = driver.find_element(by=By.XPATH, value="//meta[1]")
 print("the Xpath element is: ")
 print(find_head)
-
-time.sleep(5)
 
 find_name = driver.find_element(by=By.NAME, value="generator")
 print("the name element is: ")
